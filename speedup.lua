40e36b9018bfb5099562dd5624d4bbd33bfbc21c
tuando1506 <119750852+doanhtuanthresh@users.noreply.github.com>
2025-09-10 22:05:35 +0700

    Update speedup.lua

--- speedup.lua
+++ speedup.lua
@@ -1,15 +1,23 @@
+-- speedup.lua
 local Speed = {}
 local player = game.Players.LocalPlayer
-local humanoid = nil
 
+-- lấy humanoid
 local function getHumanoid()
     local char = player.Character or player.CharacterAdded:Wait()
     return char:WaitForChild("Humanoid")
 end
 
+-- bật speed
 function Speed.set(value)
-    humanoid = getHumanoid()
-    humanoid.WalkSpeed = value
+    local hum = getHumanoid()
+    hum.WalkSpeed = value
+end
+
+-- reset về mặc định (Roblox mặc định là 16)
+function Speed.reset()
+    local hum = getHumanoid()
+    hum.WalkSpeed = 16
 end
 
 return Speed
