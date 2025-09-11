efe749a5778484961d948b120d28f9a3f26b1d01
tuando1506 <119750852+doanhtuanthresh@users.noreply.github.com>
2025-09-11 10:32:50 +0700

    Update dungeon.lua

--- dungeon.lua
+++ dungeon.lua
@@ -1,33 +1,10 @@
--- dungeon.lua
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/source"))()
 
-local Dungeon = {}
-Dungeon.autoDungeon = false
-Dungeon.autoReturn = false
+-- Require dungeonModule
+-- ⚠️ Nhớ chỉnh đường dẫn cho đúng
+local Dungeon = require(game:GetService("ReplicatedStorage").Modules.dungeonModule)
 
--- Hàm Auto Dungeon (ví dụ giả lập, bạn sẽ thay logic riêng)
-function Dungeon.startDungeon()
-    task.spawn(function()
-        while Dungeon.autoDungeon do
-            print("[Dungeon] Auto Dungeon đang chạy...")
-            -- TODO: Thêm code farm dungeon ở đây
-            task.wait(2)
-        end
-    end)
-end
-
--- Hàm Auto Return (ví dụ giả lập, bạn sẽ thay logic riêng)
-function Dungeon.startReturn()
-    task.spawn(function()
-        while Dungeon.autoReturn do
-            print("[Dungeon] Auto Return đang chạy...")
-            -- TODO: Thêm code auto teleport về lobby
-            task.wait(2)
-        end
-    end)
-end
-
--- Tạo GUI nhỏ gọn
+-- GUI
 local Window = OrionLib:MakeWindow({
     Name = "H4xScripts",
     HidePremium = false,
@@ -41,25 +18,24 @@ local MainTab = Window:MakeTab({
     PremiumOnly = false
 })
 
+MainTab:AddLabel("Dungeon hiện tại: " .. Dungeon.detectDungeon())
+
 MainTab:AddToggle({
     Name = "Auto Dungeons",
     Default = false,
     Callback = function(Value)
         Dungeon.autoDungeon = Value
         if Value then
-            Dungeon.startDungeon()
+            Dungeon.start()
         end
     end
 })
 
 MainTab:AddToggle({
     Name = "Auto Return",
-    Default = false,
+    Default = Dungeon.autoReturn,
     Callback = function(Value)
         Dungeon.autoReturn = Value
-        if Value then
-            Dungeon.startReturn()
-        end
     end
 })
 
