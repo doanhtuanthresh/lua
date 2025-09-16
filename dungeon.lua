e7f2c66e43732c1012023bd4f2f6427573d2d799
tuando1506 <119750852+doanhtuanthresh@users.noreply.github.com>
2025-09-16 20:44:01 +0700

    Update dungeon.lua

--- dungeon.lua
+++ dungeon.lua
@@ -1,12 +1,37 @@
 local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/source"))()
-
--- Require dungeonModule
 local Dungeon = loadstring(game:HttpGet("https://raw.githubusercontent.com/doanhtuanthresh/lua/main/dungeonModule.lua"))()
 local Speed = loadstring(game:HttpGet("https://raw.githubusercontent.com/doanhtuanthresh/lua/main/speedup.lua"))()
 
--- Biến toàn cục lưu trạng thái
-getgenv().AutoDungeonState = getgenv().AutoDungeonState or false
-getgenv().AutoPlayAgainState = getgenv().AutoPlayAgainState or false
+local HttpService = game:GetService("HttpService")
+local configPath = "DungeonScript/config.json"
+
+-- Hàm đọc config nếu tồn tại
+local function loadConfig()
+    if isfile(configPath) then
+        local data = HttpService:JSONDecode(readfile(configPath))
+        return {
+            autoDungeon = data.autoDungeon or false,
+            autoPlayAgain = data.autoPlayAgain or false
+        }
+    else
+        return {
+            autoDungeon = false,
+            autoPlayAgain = false
+        }
+    end
+end
+
+-- Hàm lưu config mỗi khi thay đổi
+local function saveConfig(autoDungeon, autoPlayAgain)
+    local data = {
+        autoDungeon = autoDungeon,
+        autoPlayAgain = autoPlayAgain
+    }
+    writefile(configPath, HttpService:JSONEncode(data))
+end
+
+-- Đọc config ban đầu
+local config = loadConfig()
 
 -- GUI
 local Window = OrionLib:MakeWindow({
@@ -25,10 +50,11 @@ local MainTab = Window:MakeTab({
 -- Toggle Auto Dungeons
 MainTab:AddToggle({
     Name = "Auto Dungeons",
-    Default = getgenv().AutoDungeonState,
+    Default = config.autoDungeon,
     Callback = function(Value)
-        getgenv().AutoDungeonState = Value
         Dungeon.autoDungeon = Value
+        saveConfig(Value, config.autoPlayAgain) -- lưu lại
+        config.autoDungeon = Value
         if Value then
             Dungeon.start()
         end
@@ -38,9 +64,10 @@ MainTab:AddToggle({
 -- Toggle Auto Replay Dungeon
 MainTab:AddToggle({
     Name = "Auto Play Again",
-    Default = getgenv().AutoPlayAgainState,
+    Default = config.autoPlayAgain,
     Callback = function(Value)
-        getgenv().AutoPlayAgainState = Value
+        saveConfig(config.autoDungeon, Value) -- lưu lại
+        config.autoPlayAgain = Value
         if Value then
             Dungeon.enableAutoPlayAgain()
         else
@@ -55,20 +82,20 @@ MainTab:AddToggle({
     Default = false,
     Callback = function(Value)
         if Value then
-            Speed.set(150) -- chỉnh số tuỳ thích
+            Speed.set(150)
         else
             Speed.reset()
         end
     end
 })
 
--- Tự bật lại nếu đã bật từ lần trước
-if getgenv().AutoDungeonState then
+-- Tự bật lại trạng thái nếu config đang bật
+if config.autoDungeon then
     Dungeon.autoDungeon = true
     Dungeon.start()
 end
 
-if getgenv().AutoPlayAgainState then
+if config.autoPlayAgain then
     Dungeon.enableAutoPlayAgain()
 end
 
