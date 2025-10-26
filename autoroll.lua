c075eee33f304f46f923ec574f1cdc71726c4d5d
tuando1506 <119750852+doanhtuanthresh@users.noreply.github.com>
2025-10-26 13:08:52 +0700

    Create autoroll.lua

---
+++ autoroll.lua
@@ -0,0 +1,24 @@
+-- autoroll.lua
+local AutoRoll = {}
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RollRemote = ReplicatedStorage.Packages.Knit.Services.MedalService.RF.Roll
+
+AutoRoll.enabled = false
+AutoRoll.targetMedal = nil
+AutoRoll.delay = 1 -- giây
+
+function AutoRoll.start()
+    spawn(function()
+        while AutoRoll.enabled do
+            if RollRemote and AutoRoll.targetMedal ~= nil then
+                pcall(function()
+                    RollRemote:InvokeServer(AutoRoll.targetMedal)
+                end)
+            end
+            task.wait(AutoRoll.delay)
+        end
+    end)
+end
+
+return AutoRoll
